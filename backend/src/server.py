074cb8caa20435c0a07c1254ef3012d7cedc0aae
baanish <abhirud@ncsu.edu>
2022-02-25 10:10:16 -0500

    Removed unused imports, and added DB to gitignore

--- backend/src/server.py
+++ backend/src/server.py
@@ -6,7 +6,6 @@ import os
 load_dotenv(find_dotenv())
 
 from api import apiBlueprint
-from api.APISourceController import sourceBlueprint
 from api.APIUserController import userBlueprint
 from api.APIOrganizationController import organizationBlueprint
 
@@ -16,7 +15,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pbrDatabase.sqlite3'
 db = SQLAlchemy(app)
 
 app.register_blueprint(apiBlueprint, url_prefix='/api')
-app.register_blueprint(sourceBlueprint, url_prefix='/api/source')
 app.register_blueprint(userBlueprint, url_prefix='/api/user')
 app.register_blueprint(organizationBlueprint, url_prefix='/api/organization')
 
