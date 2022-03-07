f09106c37ab7ab31db2b1124063bb453e20ac432
jhan9 <jhan9@ncsu.edu>
2022-03-07 00:48:16 -0500

    API class for Sample Data

--- backend/src/api/APIDataController.py
+++ backend/src/api/APIDataController.py
@@ -1,17 +1,64 @@
-from flask import Blueprint
-
+from api.APIUserController import token_required
+from flask import Blueprint, jsonify, request
+from models.sample import Sample
+from models.log import createLog
 
 sampleBlueprint = Blueprint('sample', __name__)
-batchBlueprint = Blueprint('batch', __name__)
 
-@sampleBlueprint.route('/<int:item_id>', methods=['GET', 'PUT', 'DELETE', 'POST'])
-@sampleBlueprint.route('/', methods=['GET', 'POST'])
-def route_setting_sample(item_id=None):
-    from models.sample import sample
-    return Sample.fs_get_delete_put_post(item_id)
+# Creates a new sample #
+@sampleBlueprint.route('/datapoint', methods=['POST'])
+@token_required
+def create_sample():
+    newSample = Sample(request.json)
+    from server import db
+    db.session.add(newSample)
+    db.session.commit()
+    return jsonify(Sample.query.get(request.json.get('id'))), 
 
-@batchBlueprint.route('/<int:item_id>', methods=['GET', 'PUT', 'DELETE', 'POST'])
-@batchBlueprint.route('/', methods=['GET', 'POST'])
-def route_setting_batch(item_id=None):
-    from models.batch import Batch
-    return Batch.fs_get_delete_put_post(item_id)
+# Retrives specified sample #
+@sampleBlueprint.route('/datapoint/<int:item_id>', methods=['GET'])
+@token_required
+def get_sample(item_id):
+    responseJSON = jsonify(Sample.query.get(item_id))
+    if responseJSON.json is None:
+        responseJSON = jsonify({'message': 'Sample cannot be found.'})
+        return responseJSON, 404
+    else:
+        return responseJSON, 200
+
+# Retrieves all samples #
+@sampleBlueprint.route('/datapoint', methods=['GET'])
+@token_required
+def get_samples():
+    responseJSON = jsonify(Sample.query.all())
+    if responseJSON.json is None:
+        responseJSON = jsonify({'message': 'Samples cannot be returned.'})
+        return responseJSON, 404
+    else:
+        return responseJSON, 200
+
+# Deletes specified sample #
+@sampleBlueprint.route('/datapoint/<int:item_id>', methods=['DELETE'])
+@token_required
+def delete_sample(item_id):
+    if Sample.query.get(item_id) is None:
+        return jsonify({'message': 'Sample cannot be found.'}), 404
+    else:
+        from server import db
+        deletedFlock = Sample.query.get(item_id)
+        db.session.delete(Sample.query.get(item_id))
+        db.session.commit()
+        return jsonify({'message': 'Sample has been deleted'}), 200
+
+# Edits existing sample #
+@sampleBlueprint.route('/datapoint/<int:item_id>', methods=['PUT'])
+@token_required
+def edit_datapoint(item_id):
+    if Sample.query.get(item_id) is None:
+        return jsonify({'message': 'Sample cannot be found.'}), 404
+    else:
+        from server import db
+        Sample.query.filter_by(id=item_id).update(request.json)
+        db.session.commit()
+        editedSample = Sample.query.get(item_id)
+        return jsonify(editedSample), 200
