d9430fbb55203e102954c4728daff7ba0d6e13cb
abhirud <abhirud@ncsu.edu>
2022-03-07 10:41:34 -0500

    added logging to samples.

--- backend/src/api/APIDataController.py
+++ backend/src/api/APIDataController.py
@@ -2,17 +2,19 @@ from api.APIUserController import token_required
 from flask import Blueprint, jsonify, request
 from models.sample import Sample
 from models.log import createLog
+from models.enums import LogActions
 
 sampleBlueprint = Blueprint('sample', __name__)
 
 # Creates a new sample #
 @sampleBlueprint.route('/datapoint', methods=['POST'])
 @token_required
-def create_sample():
+def create_sample(current_user):
     newSample = Sample(request.json)
     from server import db
     db.session.add(newSample)
     db.session.commit()
+    createLog(current_user, LogActions.ADD_SAMPLE, 'Created new sample: ' + newSample.id)
     return jsonify(Sample.query.get(request.json.get('id'))), 
 
 # Retrives specified sample #
@@ -40,7 +42,7 @@ def get_samples():
 # Deletes specified sample #
 @sampleBlueprint.route('/datapoint/<int:item_id>', methods=['DELETE'])
 @token_required
-def delete_sample(item_id):
+def delete_sample(current_user, item_id):
     if Sample.query.get(item_id) is None:
         return jsonify({'message': 'Sample cannot be found.'}), 404
     else:
@@ -48,12 +50,13 @@ def delete_sample(item_id):
         deletedFlock = Sample.query.get(item_id)
         db.session.delete(Sample.query.get(item_id))
         db.session.commit()
+        createLog(current_user, LogActions.DELETE_SAMPLE, 'Deleted sample: ' + deletedFlock.id)
         return jsonify({'message': 'Sample has been deleted'}), 200
 
 # Edits existing sample #
 @sampleBlueprint.route('/datapoint/<int:item_id>', methods=['PUT'])
 @token_required
-def edit_datapoint(item_id):
+def edit_datapoint(current_user, item_id):
     if Sample.query.get(item_id) is None:
         return jsonify({'message': 'Sample cannot be found.'}), 404
     else:
@@ -61,4 +64,5 @@ def edit_datapoint(item_id):
         Sample.query.filter_by(id=item_id).update(request.json)
         db.session.commit()
         editedSample = Sample.query.get(item_id)
+        createLog(current_user, LogActions.EDIT_SAMPLE, 'Edited sample: ' + editedSample.id)
         return jsonify(editedSample), 200
