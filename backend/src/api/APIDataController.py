85c6b758c8084641cab95a081e921e9c37b04a33
jhan9 <jhan9@ncsu.edu>
2022-03-22 20:22:11 -0400

    Changes from before that were removed

--- backend/src/api/APIDataController.py
+++ backend/src/api/APIDataController.py
@@ -1,9 +1,5 @@
 from api.APIUserController import token_required
 from flask import Blueprint, jsonify, request
-from models.sample import Sample
-from models.batch import Batch
-from models.log import createLog
-from models.enums import LogActions
 
 sampleBlueprint = Blueprint('sample', __name__)
 batchBluePrint = Blueprint('batch', __name__)
@@ -13,17 +9,21 @@ batchBluePrint = Blueprint('batch', __name__)
 @sampleBlueprint.route('/datapoint', methods=['POST'])
 @token_required
 def create_sample(current_user):
+    from models.sample import Sample
+    from models.log import createLog
+    from models.enums import LogActions
     newSample = Sample(request.json)
     from server import db
     db.session.add(newSample)
     db.session.commit()
-    createLog(current_user, LogActions.ADD_SAMPLE, 'Created new sample: ' + newSample.id)
-    return jsonify(Sample.query.get(request.json.get('id'))), 
+    createLog(current_user, LogActions.ADD_SAMPLE, 'Created new sample: ' + str(newSample.id))
+    return jsonify(Sample.query.get(request.json.get('id'))), 201
 
 # Retrives specified sample #
 @sampleBlueprint.route('/datapoint/<int:item_id>', methods=['GET'])
 @token_required
-def get_sample(item_id):
+def get_sample(current_user,item_id):
+    from models.sample import Sample
     responseJSON = jsonify(Sample.query.get(item_id))
     if responseJSON.json is None:
         responseJSON = jsonify({'message': 'Sample cannot be found.'})
@@ -34,7 +34,8 @@ def get_sample(item_id):
 # Retrieves all samples #
 @sampleBlueprint.route('/datapoint', methods=['GET'])
 @token_required
-def get_samples():
+def get_samples(current_user):
+    from models.sample import Sample
     responseJSON = jsonify(Sample.query.all())
     if responseJSON.json is None:
         responseJSON = jsonify({'message': 'Samples cannot be returned.'})
@@ -46,6 +47,9 @@ def get_samples():
 @sampleBlueprint.route('/datapoint/<int:item_id>', methods=['DELETE'])
 @token_required
 def delete_sample(current_user, item_id):
+    from models.sample import Sample
+    from models.log import createLog
+    from models.enums import LogActions
     if Sample.query.get(item_id) is None:
         return jsonify({'message': 'Sample cannot be found.'}), 404
     else:
@@ -60,6 +64,9 @@ def delete_sample(current_user, item_id):
 @sampleBlueprint.route('/datapoint/<int:item_id>', methods=['PUT'])
 @token_required
 def edit_datapoint(current_user, item_id):
+    from models.sample import Sample
+    from models.log import createLog
+    from models.enums import LogActions
     if Sample.query.get(item_id) is None:
         return jsonify({'message': 'Sample cannot be found.'}), 404
     else:
@@ -67,7 +74,7 @@ def edit_datapoint(current_user, item_id):
         Sample.query.filter_by(id=item_id).update(request.json)
         db.session.commit()
         editedSample = Sample.query.get(item_id)
-        createLog(current_user, LogActions.EDIT_SAMPLE, 'Edited sample: ' + editedSample.id)
+        createLog(current_user, LogActions.EDIT_SAMPLE, 'Edited sample: ' + str(editedSample.id))
         return jsonify(editedSample), 200
 
 
@@ -77,6 +84,9 @@ def edit_datapoint(current_user, item_id):
 @token_required
 # batch_data is a BatchData.JSON
 def create_batchdata(current_user, batch_data):
+    from models.batch import Batch
+    from models.log import createLog
+    from models.enums import LogActions
     from server import db
     db.session.add(batch_data)
     db.session.commit()
@@ -86,7 +96,8 @@ def create_batchdata(current_user, batch_data):
 # Retrieves all batches #
 @batchBluePrint.route('/datapoint/batch', methods=['GET'])
 @token_required
-def get_batches():
+def get_batches(current_user):
+    from models.batch import Batch
     responseJSON = jsonify(Batch.query.all())
     if responseJSON.json is None:
         responseJSON = jsonify({'message': 'Batches cannot be returned.'})
@@ -97,7 +108,8 @@ def get_batches():
 # Retrieves specifed batch #
 @batchBluePrint.route('/datapoint/batch/<int:item_id>', methods=['GET'])
 @token_required
-def get_batch(item_id):
+def get_batch(current_user, item_id):
+    from models.batch import Batch
     responseJSON = jsonify(Batch.query.get(item_id))
     if responseJSON.json is None:
         responseJSON = jsonify({'message': 'Batch cannot be found.'})
@@ -109,6 +121,9 @@ def get_batch(item_id):
 @batchBluePrint.route('/datapoint/batch/<int:item_id>', methods=['PUT'])
 @token_required
 def edit_batch(current_user, item_id):
+    from models.batch import Batch
+    from models.log import createLog
+    from models.enums import LogActions
     if Batch.query.get(item_id) is None:
         return jsonify({'message': 'Batch cannot be found.'}), 404
     else:
@@ -123,6 +138,9 @@ def edit_batch(current_user, item_id):
 @batchBluePrint.route('/datapoint/batch/<int:item_id>', methods=['DELETE'])
 @token_required
 def delete_batch(current_user, item_id):
+    from models.batch import Batch
+    from models.log import createLog
+    from models.enums import LogActions
     if Batch.query.get(item_id) is None:
         return jsonify({'message': 'Batch cannot be found.'}), 404
     else:
