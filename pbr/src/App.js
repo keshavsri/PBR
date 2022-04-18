41280001775460d6165f60af28c4a85be58afdba
arpenny <arpenny@ncsu.edu>
2022-04-18 14:11:15 -0400

    Fixed issue with auth loading from context

--- pbr/src/App.js
+++ pbr/src/App.js
@@ -18,71 +18,44 @@ import { Box, CircularProgress, Grid } from "@mui/material";
 function App() {
   const routing = useRoutes(routes);
 
-  const { loadingAuth } = useAuth();
-
-  if (loadingAuth) {
-    return (
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <ThemeProvider theme={mainTheme}>
-          <AuthProvider>
-            <>
-              <GlobalStyles />
-              <AuthConsumer>
-                {/* {!context.loadingAuth && { routing }}
-                {context.loadingAuth && (
-                  <Grid
-                    container
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
-                    sx={{ height: "100vh", backgroundColor: "white" }}
-                  >
-                    <Box sx={{ display: "flex" }}>
-                      <CircularProgress
-                        color="primary"
-                        sx={{
-                          width: "100px !IMPORTANT",
-                          height: "100px !IMPORTANT",
-                        }}
-                      />
-                    </Box>
-                  </Grid>
-                )} */}
-
-                {/* OR */}
-
-                {(context) => {
-                  if (context.loadingAuth) {
-                    return (
-                      <Grid
-                        container
-                        direction="row"
-                        justifyContent="center"
-                        alignItems="center"
-                        sx={{ height: "100vh", backgroundColor: "white" }}
-                      >
-                        <Box sx={{ display: "flex" }}>
-                          <CircularProgress
-                            color="primary"
-                            sx={{
-                              width: "100px !IMPORTANT",
-                              height: "100px !IMPORTANT",
-                            }}
-                          />
-                        </Box>
-                      </Grid>
-                    );
-                  } else {
-                    return { routing };
-                  }
-                }}
-              </AuthConsumer>
-            </>
-          </AuthProvider>
-        </ThemeProvider>
-      </LocalizationProvider>
-    );
-  }
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <ThemeProvider theme={mainTheme}>
+        <AuthProvider>
+          <>
+            <GlobalStyles />
+            <AuthConsumer>
+              {(context) => {
+                if (context.loadingAuth) {
+                  return (
+                    <Grid
+                      container
+                      direction="row"
+                      justifyContent="center"
+                      alignItems="center"
+                      sx={{ height: "100vh", backgroundColor: "white" }}
+                    >
+                      <Box sx={{ display: "flex" }}>
+                        <CircularProgress
+                          color="primary"
+                          sx={{
+                            width: "100px !IMPORTANT",
+                            height: "100px !IMPORTANT",
+                          }}
+                        />
+                      </Box>
+                    </Grid>
+                  );
+                } else {
+                  return <>{routing}</>;
+                }
+              }}
+            </AuthConsumer>
+          </>
+        </AuthProvider>
+      </ThemeProvider>
+    </LocalizationProvider>
+  );
 }
 
 export default App;
