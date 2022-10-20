32c0705bd5cf81df5383afcd5ffe07951fa593c6
Jack Boike <jjboike@ncsu.edu>
2022-10-20 17:58:58 -0400

    Added database modifications in new_schemas.py and new_models.py

--- backend/src/Models.py
+++ backend/src/Models.py
@@ -458,16 +458,3 @@ class Batch(db.Model):
     sample = db.relationship('Sample', backref='Batch')
 
 
-def createLog(current_user, action, logContent):
-    """ This function creates a log entry.
-
-    It takes the current user, the action that was performed, and the content of the action.
-
-    Args:
-        current_user: The user who did the action
-        action: The action that was performed
-        logContent: A more readable version of the action with an ID or name of the affected object
-    """
-    log = Log(current_user.id, current_user.organization_id, current_user.role, action, logContent)
-    db.session.add(log)
-    db.session.commit()
---
+++ backend/src/helpers/log.py
@@ -0,0 +1,13 @@
+def createLog(current_user, action, logContent):
+    """ This function creates a log entry.
+
+    It takes the current user, the action that was performed, and the content of the action.
+
+    Args:
+        current_user: The user who did the action
+        action: The action that was performed
+        logContent: A more readable version of the action with an ID or name of the affected object
+    """
+    log = Log(current_user.id, current_user.organization_id, current_user.role, action, logContent)
+    db.session.add(log)
+    db.session.commit()
