dcbe9213904ee89b60e006bc87eef3f18dbb8e18
vravich <vravich@ncsu.edu>
2022-10-26 10:51:03 -0400

    Created analyte blueprint and added other blueprints to flask app

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 from src.api import apiBlueprint
-from src.api.user import userBlueprint
-from src.api.sample import sampleBlueprint
-from src.api.organization import organizationBlueprint
 from src.api.log import logBlueprint
+from src.api.user import userBlueprint
 from src.api.flock import flockBlueprint
+from src.api.sample import sampleBlueprint
+from src.api.analyte import analyteBlueprint
 from src.api.machine import machineBlueprint
+from src.api.machine_type import machineTypeBlueprint
+from src.api.organization import organizationBlueprint
+
 import os
 from dotenv import load_dotenv, find_dotenv
 from flask.cli import with_appcontext
@@ -17,12 +21,13 @@ load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 app.register_blueprint(apiBlueprint, url_prefix='/api')
-app.register_blueprint(userBlueprint, url_prefix='/api/user')
-app.register_blueprint(organizationBlueprint, url_prefix='/api/organization')
 app.register_blueprint(logBlueprint, url_prefix='/api/log')
+app.register_blueprint(userBlueprint, url_prefix='/api/user')
 app.register_blueprint(flockBlueprint, url_prefix='/api/flock')
 app.register_blueprint(sampleBlueprint, url_prefix='/api/sample')
 app.register_blueprint(machineBlueprint, url_prefix='/api/machine')
+app.register_blueprint(machineTypeBlueprint, url_prefix='/api/machinetype')
+app.register_blueprint(organizationBlueprint, url_prefix='/api/organization')
 app.config['SECRET_KEY'] = os.environ.get("JWT_SECRET")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
