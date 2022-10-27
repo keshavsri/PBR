922e5992a7dbdcc6db931df1d866efdba027a942
Jack Boike <jjboike@ncsu.edu>
2022-10-27 15:51:27 -0400

    Added fix for machines api

--- backend/src/api/machine.py
+++ backend/src/api/machine.py
@@ -5,7 +5,7 @@ from src import models, schemas
 from src.enums import Roles, LogActions
 from src.helpers import log
 
-
+from src.helpers import machine
 machineBlueprint = Blueprint('machine', __name__)
 
 
@@ -25,6 +25,18 @@ def get_machines(access_allowed, current_user, given_org_id=None):
     """
 
     if access_allowed:
+        # response json is created here and gets returned at the end of the block for GET requests.
+        responseJSON = None
+        current_Organization = current_user.organization_id
+
+        if given_org_id:
+            if current_user.role == Roles.Super_Admin:
+                responseJSON = src.helpers.machine.get_machines_by_org(current_Organization)
+            elif current_user.organization_id == given_org_id:
+                responseJSON = src.helpers.machine.get_machines_by_org(current_Organization)
+            else:
+                responseJSON = jsonify({'message': 'Insufficient Permissions'})
+                return responseJSON, 401
         if given_org_id is None:
             return jsonify({'message': 'Organization ID must be specified'}), 400
         elif current_user.organization_id == given_org_id or current_user.role == Roles.Super_Admin:
