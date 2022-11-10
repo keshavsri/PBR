397afabaa7f05fb279b30ab5a97083c5689f8707
Mohammed Boukhris <mboukhr@ncsu.edu>
2022-11-09 19:09:25 -0500

    Edit sample modal progress

--- pbr/src/components/ManageData/ViewData/DataView.js
+++ pbr/src/components/ManageData/ViewData/DataView.js
@@ -370,11 +370,8 @@ export default function DataView() {
               {editSampleModalVisiblity ? (
                 <EditSampleModal
                   selected={selected}
-                  submitAll={submitAll}
-                  submitOne={submitOne}
-                  rows={sampleList}
                   selectedSamples={selectedSamples}
-                  setSelectedSamples={setSelectedSamples}
+                  Organization={organization}
                   editSampleModalVisiblity={editSampleModalVisiblity}
                   setEditSampleModalVisibility={setEditSampleModalVisibility}
                 />
