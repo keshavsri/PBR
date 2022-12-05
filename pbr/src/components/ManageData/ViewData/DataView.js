e1d7872bd676f7e0717e42b72ba0d63f317e1c03
Jack Boike <jjboike@ncsu.edu>
2022-12-05 10:33:45 -0500

    Removed console.log

--- pbr/src/components/ManageData/ViewData/DataView.js
+++ pbr/src/components/ManageData/ViewData/DataView.js
@@ -359,7 +359,6 @@ export default function DataView() {
   React.useEffect(() => {
     setSelected([]);
     setSelectedSamples([]);
-    console.log("Full Row List");
   }, [fullRowList]);
 
   React.useEffect(() => {
